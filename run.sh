ca3a35d92388e0c81b81b86e02645576b0555e08
miooodek <miooodek@gmail.com>
2015-11-07 18:52:42 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 chown www-data:www-data /app -R
 
-if [ "$ALLOW_OVERRIDE" = "**False**" ]; then
-    unset ALLOW_OVERRIDE
-else
-    sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
-    a2enmod rewrite
-fi
+sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
+a2enmod rewrite
 
 source /etc/apache2/envvars
 tail -F /var/log/apache2/* &
